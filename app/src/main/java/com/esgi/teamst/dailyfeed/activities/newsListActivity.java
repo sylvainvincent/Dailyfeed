ee5bfc6f648d527ebf03c804bbeeee13f809ef41
sylvainvincent <sylvain91130@hotmail.fr>
2016-07-12 23:41:08 +0200

    Ajout de services et du boutons refresh + filtre

--- app/src/main/java/com/esgi/teamst/dailyfeed/activities/newsListActivity.java
+++ app/src/main/java/com/esgi/teamst/dailyfeed/activities/newsListActivity.java
@@ -1,22 +1,27 @@
 package com.esgi.teamst.dailyfeed.activities;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.esgi.teamst.dailyfeed.R;
+import com.esgi.teamst.dailyfeed.dao.SourceDAO;
 import com.esgi.teamst.dailyfeed.models.Article;
+import com.esgi.teamst.dailyfeed.models.Source;
+import com.esgi.teamst.dailyfeed.services.ArticlesService;
 import com.esgi.teamst.dailyfeed.xmlHandler.DBArticleHandler;
-import com.esgi.teamst.dailyfeed.xmlHandler.DisplayListHandler;
-import com.esgi.teamst.dailyfeed.xmlHandler.XMLParseHandler;
 import com.getbase.floatingactionbutton.FloatingActionButton;
 
-import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Guideline utilisé : https://github.com/ribot/android-guidelines/blob/master/project_and_code_guidelines.md
@@ -27,29 +32,51 @@ public class newsListActivity extends AppCompatActivity implements AdapterView.O
     public static final String TAG = newsListActivity.class.getSimpleName();
     public static final String EXTRA_ARTICLE_ID = "com.esgi.teamst.dailyfeed.EXTRA_ARTICLE_ID";
     public static int mUserId;
-    protected FloatingActionButton mFabDisconnection;
-    protected FloatingActionButton mFabFavoritesList;
-    ListView mListViewArticlesMain;
-    SharedPreferences prefs = null;
+
+    private FloatingActionButton mFabDisconnection;
+    private FloatingActionButton mFabFavoritesList;
+    private FloatingActionButton mFabFilter;
+    private ListView mListViewArticlesMain;
+
+    private SharedPreferences mPrefs = null;
+    private SourceDAO mSourceDAO;
+    private String[] mSourceNames;
+    private boolean[] mSourceBoolean;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.setContentView(R.layout.activity_news_list);
         this.initViews();
+        Intent intent = new Intent(this, ArticlesService.class);
+        startService(intent);
         mUserId = getIntent().getIntExtra(MainActivity.EXTRA_USER_ID, -1);
-        prefs = getSharedPreferences("com.esgi.teamst.dailyfeed", MODE_PRIVATE);
+        mPrefs = getSharedPreferences("com.esgi.teamst.dailyfeed", MODE_PRIVATE);
+        mSourceDAO = new SourceDAO(this);
+        mSourceDAO.open();
+        List<Source> sourceList = mSourceDAO.getAllSource();
+        mSourceDAO.close();
+        mSourceNames = new String[sourceList.size()];
+        mSourceBoolean = new boolean[sourceList.size()];
+        int i = 0;
+        for(Source source : sourceList){
+            mSourceNames[i] = source.getName();
+            mSourceBoolean[i] = true;
+            i++;
+        }
+
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        if (prefs.getBoolean("firstrun", true)) {
+        if (mPrefs.getBoolean("firstrun", true)) {
             // Do first run stuff here then set 'firstrun' as false
             new DBArticleHandler(mListViewArticlesMain,newsListActivity.this).execute(true);
-            // using the following line to edit/commit prefs
-            prefs.edit().putBoolean("firstrun", false).commit();
+            // using the following line to edit/commit mPrefs
+            mPrefs.edit().putBoolean("firstrun", false).commit();
         }
         else {
             new DBArticleHandler(mListViewArticlesMain,newsListActivity.this).execute(false);
@@ -71,6 +98,26 @@ public class newsListActivity extends AppCompatActivity implements AdapterView.O
     public void onClick(View v) {
         Intent intent = null;
         switch (v.getId()){
+            case R.id.fab_refresh:
+                // TODO: 12/07/16 Mettre ici la fonction pour actualiser la liste
+                break;
+            case R.id.fab_filter:
+                AlertDialog builder = new AlertDialog.Builder(this)
+                .setMultiChoiceItems(mSourceNames, mSourceBoolean, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+
+                    }
+                })
+                .setPositiveButton(getString(android.R.string.yes), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .create();
+                builder.show();
+                break;
             case R.id.fab_disconnection:
                 intent = new Intent(this, MainActivity.class);
                 finish();
@@ -87,7 +134,19 @@ public class newsListActivity extends AppCompatActivity implements AdapterView.O
             mFabDisconnection.setOnClickListener(this);
         mFabFavoritesList = (FloatingActionButton) findViewById(R.id.fab_favorites_list);
             mFabFavoritesList.setOnClickListener(this);
+        mFabFilter = (FloatingActionButton) findViewById(R.id.fab_filter);
+            mFabFilter.setOnClickListener(this);
         mListViewArticlesMain = (ListView) findViewById(R.id.list_articles);
         mListViewArticlesMain.setOnItemClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+
+
     }
 }
---
+++ app/src/main/java/com/esgi/teamst/dailyfeed/services/ArticlesService.java
@@ -0,0 +1,84 @@
+package com.esgi.teamst.dailyfeed.services;
+
+import android.app.KeyguardManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.PowerManager;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.esgi.teamst.dailyfeed.dao.ArticleDAO;
+import com.esgi.teamst.dailyfeed.models.Article;
+
+import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by sylvainvincent on 09/07/16.
+ */
+public class ArticlesService extends Service {
+
+    private static final String TAG = ArticlesService.class.getSimpleName();
+    private ArticleDAO mArticleDao;
+    private String mProgrammingId;
+    private Timer timer;
+    private Calendar c;
+    TimerTask timerTask = new TimerTask() {
+        @Override
+        public void run() {
+            test();
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Toast.makeText(getApplicationContext(), "oncreate", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "running", Toast.LENGTH_LONG).show();
+        Log.i(TAG, "onStartCommand: running");
+        mProgrammingId = intent.getStringExtra("id");
+        if (timer == null) {
+            timer = new Timer();
+            // On commence dans 10 secondes et on actualise toutes les minutes
+            timer.schedule(timerTask, 10000, 60000);
+        } else {
+            timer.cancel();
+            timerTask.cancel();
+            timer.schedule(timerTask, 10000, 60000);
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        Toast.makeText(this, "test", Toast.LENGTH_LONG).show();
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy: destroy");
+        try {
+            timer.cancel();
+            timerTask.cancel();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void test() {
+        Log.i(TAG, "test: TEST");
+        // TODO: 12/07/16 Actualiser la liste avec les nouveaux articles
+    }
+}
